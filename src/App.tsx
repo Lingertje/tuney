b8bbe6081c7a44dc47f8b1422f7b463d30b35c5c
Mart Linger <mart.linger@indivirtual.com>
2021-12-20 21:19:03 +0100

    Remove favorites for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FC, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { FavoritesProvider } from "context/FavoriteContext";
 
 const Movies = lazy(() => import("routes/Movies"));
 const Series = lazy(() => import("routes/Series"));
@@ -14,19 +13,17 @@ const App: FC = () => {
 	return (
 		<>
 			<Router>
-				<FavoritesProvider>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Routes>
-							<Route path="/movie/:slug" element={<GenericDetail category="movie" />} />
-							<Route path="/movies" element={<Movies />} />
-							<Route path="/tv/:slug" element={<GenericDetail category="tv" />} />
-							<Route path="/series" element={<Series />} />
-							<Route path="/favorites" element={<Favorites />} />
-							<Route path="/search" element={<Search />} />
-							<Route path="/" element={<Homepage />} />
-						</Routes>
-					</Suspense>
-				</FavoritesProvider>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path="/movie/:slug" element={<GenericDetail category="movie" />} />
+						<Route path="/movies" element={<Movies />} />
+						<Route path="/tv/:slug" element={<GenericDetail category="tv" />} />
+						<Route path="/series" element={<Series />} />
+						<Route path="/favorites" element={<Favorites />} />
+						<Route path="/search" element={<Search />} />
+						<Route path="/" element={<Homepage />} />
+					</Routes>
+				</Suspense>
 			</Router>
 			<ReactQueryDevtools />
 		</>
--- src/components/Poster/Poster.tsx
+++ src/components/Poster/Poster.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import { Link } from "react-router-dom";
 
 import { Movie } from "types/Movie";
